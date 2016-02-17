8713892a8386835671d7d8090a738e6e72d90a31
chris marlowe <chris@codechris.com>
2016-02-17 04:49:13 -0600

    added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,9 @@ socket.on("connect", function() {
 	console.log("connected to socket IO server!");
 	
 });
+
+socket.on("message", function (message){
+	console.log("new message");
+	console.log(message.text);
+	
+})
